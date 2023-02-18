f45bc9899071a713ac5db72c40c80f25d50152dc
MoonS <99530280+MoonS1120@users.noreply.github.com>
2023-02-19 00:12:36 +0800

    Update personal _statement_test.py

--- personal _statement_test.py
+++ personal _statement_test.py
@@ -2,11 +2,9 @@ import openai
 
 openai.api_key = "API-KEY"
 
-prompt="Write a paragraph using these information. \'Year 11 Student\',\'Joined Garden international school in Year 4\', \'Passionate about STEM\', \'hope to contribute to the school community in a positive way\'"
-
 completions = openai.Completion.create(
     engine="text-davinci-002",
-    prompt=prompt
+    prompt="Write a paragraph using these information. \'Year 11 Student\',\'Joined Garden international school in Year 4\', \'Passionate about STEM\', \'hope to contribute to the school community in a positive way\'"
     temperature=0.2,
     max_tokens=1024,
 )
